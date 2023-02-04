af57eaeb18891b56ac4b542a3a316615981abebc
haidangcs14 <haidang9ahv1@gmail.com>
2023-02-04 11:06:29 +0700

    add new

--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 
 # Create your views here.
 def index(request):
     # return HttpResponse('<h1>Hey, Welcome</h1>')
-    context = {
-        'name': 'Hai Dang',
-        'age': 23,
-        'nationality': 'Vietnam'
-    }
-    return render(request, 'index.html', context)
+    ft1 = Feature()
+    ft1.id = 0
+    ft1.name = 'Fast'
+    ft1.details = 'Our service is very quick'
+    return render(request, 'index.html', {'feature': ft1})
+
+def counter(req):
+    text = req.POST['text']
+    amount_of_words = len(text.split())
+    return render(req, 'counter.html', {'amount': amount_of_words})
